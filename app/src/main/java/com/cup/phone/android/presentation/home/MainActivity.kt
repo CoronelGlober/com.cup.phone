7e756c662c184f6a7473a04a148ab864e9c8ca6d
David Coronel <david.coronel@autodesk.com>
2021-03-15 05:22:23 -0500

    added view binding

--- app/src/main/java/com/cup/phone/android/presentation/home/MainActivity.kt
+++ app/src/main/java/com/cup/phone/android/presentation/home/MainActivity.kt
@@ -2,16 +2,20 @@ package com.cup.phone.android.presentation.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.airbnb.epoxy.EpoxyRecyclerView
 import com.cup.phone.android.R
+import com.cup.phone.android.databinding.ActivityMainBinding
 import com.cup.phone.core.data.di.Locator
 import com.cup.phone.core.presentation.MessagesPresenter
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -19,35 +23,38 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
 
     lateinit var messagesPresenter: MessagesPresenter
-    lateinit var messageField: EditText
+
+    lateinit var content: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recycler = findViewById<EpoxyRecyclerView>(R.id.messagesRecycler)
-        recycler.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, true)
+        content = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        content.messagesRecycler.layoutManager = LinearLayoutManager(this, VERTICAL, true)
         messagesPresenter = MessagesPresenter(Locator.getRepository(), Locator.getClient())
-        messageField = findViewById<EditText>(R.id.messageField)
 
         lifecycleScope.launchWhenResumed {
-            messagesPresenter.getMessages().collect { elements ->
-                withContext(Dispatchers.Main) {
-                    recycler.setModels(
-                        elements.map {
-                            MessageModel_()
-                                .id(it.message)
-                                .messageItem(it)
-                        }
-                    )
-                    recycler.scrollToPosition(0);
+            messagesPresenter.getMessages()
+                .catch { println(it.message) }
+                .collect { elements ->
+                    withContext(Dispatchers.Main) {
+                        content.messagesRecycler.setModels(
+                            elements.map {
+                                MessageModel_()
+                                    .id(it.message)
+                                    .messageItem(it)
+                            }
+                        )
+                        content.messagesRecycler.scrollToPosition(0);
+                    }
                 }
-            }
         }
     }
 
 
     fun sendMessage(view: View) {
-        messagesPresenter.sendMessage(messageField.text.toString())
-        messageField.text.clear()
+        messagesPresenter.sendMessage(content.messageField.text.toString())
+        content.messageField.text.clear()
     }
 }
