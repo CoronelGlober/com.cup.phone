aaab76a0deeb2f05403780066e539f5142aad81f
Federico Lagarmilla <flagarmilla@gmail.com>
2021-03-17 01:58:16 -0300

    Implement date formatter

--- core/src/iosMain/kotlin/com/cup/phone/core/presentation/formatter/DateFormatter.kt
+++ core/src/iosMain/kotlin/com/cup/phone/core/presentation/formatter/DateFormatter.kt
@@ -1,11 +1,19 @@
 package com.cup.phone.core.presentation.formatter
 
+import platform.Foundation.NSDate
+import platform.Foundation.NSTimeInterval
+import platform.Foundation.dateWithTimeIntervalSince1970
+import platform.Foundation.NSDateFormatter
+
 actual class DateFormatter {
     companion object {
         const val TAG = "DateFormatter"
     }
 
     actual fun parseDate(timestamp: Long): String {
-        return ""
+        val date = NSDate.dateWithTimeIntervalSince1970(timestamp as NSTimeInterval)
+        val dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss a"
+        return dateFormatter.stringFromDate(date)
     }
 }
