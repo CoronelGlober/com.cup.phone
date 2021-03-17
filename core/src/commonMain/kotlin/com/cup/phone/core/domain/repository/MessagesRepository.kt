46a2cd6b2fe3fc451a9e9cb13a5e4bc64781cd26
Federico Lagarmilla <flagarmilla@gmail.com>
2021-03-17 10:47:36 -0300

    get message helpers. iOS code

--- core/src/commonMain/kotlin/com/cup/phone/core/domain/repository/MessagesRepository.kt
+++ core/src/commonMain/kotlin/com/cup/phone/core/domain/repository/MessagesRepository.kt
@@ -1,11 +1,12 @@
 package com.cup.phone.core.domain.repository
 
 import com.cup.phone.core.data.dto.RawMessage
+import com.cup.phone.core.data.repository.CommonFlow
 import com.cup.phone.core.domain.entities.Message
 import kotlinx.coroutines.flow.Flow
 
 interface MessagesRepository {
     fun getMessages(): Flow<List<Message>>
-
+    fun getMessagesHelper(): CommonFlow<List<Message>>
     fun addMessage(message: RawMessage)
 }
